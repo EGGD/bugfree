8aadee43b32e79abe7090bc0540576fbcbc9a9d3
EGGD <772319837@qq.com>
2017-09-16 10:47:51 +0800

    no message

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HomePage from './js/HomePage';
+ReactDOM.render(<HomePage/> ,document.getElementById('bugfree'))
---
+++ src/js/Header.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+
+class Header extends Component {
+
+    render(){
+        return(
+            <div>
+                <span>未修复</span>
+                <span>已修复</span>
+                <span>新建</span>
+            </div>
+        )
+    }
+}
+
+export default Header;
